500064ebc6be78e0870f87b509199eb71089bd58
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-05-16 21:38:27 +0300

    Initial commit

---
+++ app/src/main/java/com/lost/chatapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.lost.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message").child("Tom")
+
+        myRef.setValue("Hello, World!")
+
+    }
+}
