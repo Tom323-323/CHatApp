7753d949e4656543a2b87f165551661830527a7a
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-05-16 21:58:10 +0300

    16/05 - Test setValue and value.

--- app/src/main/java/com/lost/chatapp/MainActivity.kt
+++ app/src/main/java/com/lost/chatapp/MainActivity.kt
@@ -2,18 +2,49 @@ package com.lost.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.lost.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = Firebase.database
         val myRef = database.getReference("message").child("Tom")
 
-        myRef.setValue("Hello, World!")
+        binding.button.setOnClickListener(View.OnClickListener {
+            val data = binding.edText.text
+            myRef.setValue("$data")
 
+        })
+
+        onResumeData(myRef)
+
+    }
+
+    private fun onResumeData(referenceDB: DatabaseReference){
+        referenceDB.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    textToDisplay.append("\n")
+                    textToDisplay.append(snapshot.value.toString())
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
